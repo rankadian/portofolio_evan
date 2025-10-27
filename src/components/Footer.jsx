bdb2d764301330a01272c926008f27661d8b783d
rankadian <dianthafafian@gmail.com>
2025-10-27 10:33:57 +0700

    update href sosial media

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,38 +4,59 @@ import { VscHome, VscArchive, VscAccount } from "react-icons/vsc";
 
 const Footer = () => {
   const items = [
-    { icon: <VscHome size={18} />, label: "Home", onClick: () => document.getElementById("home")?.scrollIntoView({ behavior: "smooth" }) },
-    { icon: <VscAccount size={18} />, label: "About Me", onClick: () => document.getElementById("about")?.scrollIntoView({ behavior: "smooth" }) },
-    { icon: <VscArchive size={18} />, label: "Project", onClick: () => document.getElementById("project")?.scrollIntoView({ behavior: "smooth" }) },
+    {
+      icon: <VscHome size={18} />,
+      label: "Home",
+      onClick: () =>
+        document.getElementById("home")?.scrollIntoView({ behavior: "smooth" }),
+    },
+    {
+      icon: <VscAccount size={18} />,
+      label: "About Me",
+      onClick: () =>
+        document
+          .getElementById("about")
+          ?.scrollIntoView({ behavior: "smooth" }),
+    },
+    {
+      icon: <VscArchive size={18} />,
+      label: "Project",
+      onClick: () =>
+        document
+          .getElementById("project")
+          ?.scrollIntoView({ behavior: "smooth" }),
+    },
   ];
 
   return (
     <div className="mt-32 pb-8 flex flex-col items-center relative z-10">
       {/* Flex container adaptif */}
       <div className="w-full flex flex-col md:flex-row items-center md:justify-between gap-6">
-        
         {/* Judul - paling atas di mobile */}
-        <h1 className="text-2xl font-bold order-1 md:order-none">
-          Portofolio
-        </h1>
+        <h1 className="text-2xl font-bold order-1 md:order-none">Portofolio</h1>
 
         {/* Ikon Sosmed - di tengah di mobile */}
         <div className="flex gap-3 order-2 md:order-none">
-          <a href="https://github.com/rissss21"><i className="ri-github-fill ri-2x"></i></a>
-          <a href="https://www.instagram.com/farisedrikprayoga/"><i className="ri-instagram-fill ri-2x"></i></a>
-          <a href="https://www.youtube.com/@FarisEdrikPrayoga"><i className="ri-youtube-fill ri-2x"></i></a>
+          <a href="https://github.com/rankadian">
+            <i className="ri-github-fill ri-2x"></i>
+          </a>
+          <a href="https://www.instagram.com/evann_dn/">
+            <i className="ri-instagram-fill ri-2x"></i>
+          </a>
+          <a href="https://www.youtube.com/@dianthafafian188">
+            <i className="ri-youtube-fill ri-2x"></i>
+          </a>
         </div>
 
         {/* Dock - paling bawah di mobile */}
         <div className="order-3 md:order-none mt-15 md:mt-0  md:mb-0">
-          <Dock 
+          <Dock
             items={items}
             panelHeight={30}
             baseItemSize={60}
             magnification={100}
           />
         </div>
-
       </div>
     </div>
   );
